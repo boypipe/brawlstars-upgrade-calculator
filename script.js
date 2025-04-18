92c95f7e7b438780b994a2a7f5ca36f7ccb6aaa8
boypipe <kaipong_yip@yahoo.com>
2025-04-18 21:57:03 +0800

    Minor fixes to design

--- script.js
+++ script.js
@@ -9,16 +9,18 @@ document.getElementById('calculate').addEventListener('click', function () {
     const hypercharge = parseInt(document.getElementById('hypercharge').value) || 0;
     const brawlers = parseInt(document.getElementById('brawlers').value) || 1;
 
-    if (startLevel && endLevel && endLevel >= startLevel && startLevel >= 1 && endLevel <= 11) {
+    if ((startLevel && endLevel && endLevel >= startLevel && startLevel >= 1 && endLevel <= 11) || (!startLevel && !endLevel)) {
         const powerPointsCost = [0, 20, 30, 50, 80, 130, 210, 340, 550, 890, 1440];
         const coinsCost = [0, 20, 35, 75, 140, 290, 480, 800, 1250, 1875, 2800];
 
         let totalPowerPoints = 0;
         let totalCoins = 0;
 
-        for (let i = startLevel; i < endLevel; i++) {
-            totalPowerPoints += powerPointsCost[i];
-            totalCoins += coinsCost[i];
+        if (startLevel && endLevel) {
+            for (let i = startLevel; i < endLevel; i++) {
+                totalPowerPoints += powerPointsCost[i];
+                totalCoins += coinsCost[i];
+            }
         }
 
         let totalCost = (totalCoins * brawlers) + (gadgets * 1000 + starPowers * 2000 + gears * 1000 + epicGears * 1500 + mythicGears * 2000 + hypercharge * 5000);
